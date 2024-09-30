189b284087e2227b9d14bae73540768e95c2c172
chamudith bandara <chamudithbuddima@gmail.com>
2024-09-30 21:06:11 +0530

    Sign Up

--- App.js
+++ App.js
@@ -10,11 +10,9 @@ import {
   Image,
 } from "react-native";
 import { useEffect } from "react";
-import { FontAwesome } from "@expo/vector-icons";
+import { FontAwesome6 } from "@expo/vector-icons";
 
 SplashScreen.preventAutoHideAsync();
-
-const mainImagePath = require("./assets/images/main.jpeg")
 
 export default function App() {
   const [loaded, error] = useFonts({
@@ -33,31 +31,39 @@ export default function App() {
     return null;
   }
 
+  const logoPath = require("./assets/images/main.jpeg");
+
+
   return (
     <View style={stylesheet.view1}>
 
-      <Image style={stylesheet.image1} source={mainImagePath}/>
+      <Image style={stylesheet.image1} source={logoPath}/>
+
+      <Text style={stylesheet.text1}>Create Account</Text>
+
+      <Text style={stylesheet.text2}>Hello! Welcome to Smart Chat</Text>
+
+      <Text style={stylesheet.text3}>Mobile</Text>
+      <TextInput style={stylesheet.input1} inputMode={"tel"}/>
+
+      <Text style={stylesheet.text3}>First Name</Text>
+      <TextInput style={stylesheet.input1} inputMode={"text"}/>
+
+      <Text style={stylesheet.text3}>Last Name</Text>
+      <TextInput style={stylesheet.input1} inputMode={"text"}/>
+
+      <Text style={stylesheet.text3}>Password</Text>
+      <TextInput style={stylesheet.input1} secureTextEntry={true} inputMode={"text"}/>
       
-      <Text style={stylesheet.text1}>Sign In</Text>
-      <Text style={stylesheet.text2}>
-        Hello! Welcome to Smart Chat. Please fill your details to continue
-      </Text>
-
-      <Text style={stylesheet.text4}>Mobile</Text>
-      <TextInput style={stylesheet.input1} inputMode={"tel"} />
-
-      <Text style={stylesheet.text4}>Password</Text>
-      <TextInput style={stylesheet.input1} secureTextEntry={true} />
-
-      <Pressable
-        style={stylesheet.pressable1}
-        onPress={() => {
-          Alert.alert("Testing", "Success");
-        }}
-      >
-        <FontAwesome name={"paper-plane"} size={18} color={"white"}/>
-        <Text style={stylesheet.text3}>Sign In</Text>
+      <Pressable style={stylesheet.Pressable1}>
+        <FontAwesome6 name={"right-to-bracket"} color={"white"} size={20}/>
+        <Text style={stylesheet.text4}>Sign Up</Text>
       </Pressable>
+
+      <Pressable style={stylesheet.Pressable2}>
+        <Text style={stylesheet.text5}>Already Registered? Go To Sign In</Text>
+      </Pressable>
+
     </View>
   );
 }
@@ -65,62 +71,72 @@ export default function App() {
 const stylesheet = StyleSheet.create({
   view1: {
     flex: 1,
-    rowGap: 15,
-    paddingHorizontal: 25,
-    justifyContent: "center",
+    justifyContent:"center",
+    paddingHorizontal:20,
+    rowGap:10,
   },
 
-  text1: {
-    fontSize: 28,
-    color: "#023e8a",
-    fontFamily: "Montserrat-Bold",
+  text1:{
+    fontSize:32,
+    fontFamily:"Montserrat-Bold",
+    textAlign:"center",
   },
 
-  text2: {
-    fontSize: 18,
-    fontFamily: "Montserrat-Regular",
+  text2:{
+    fontSize:20,
+    fontFamily:"Montserrat-Regular",
+    textAlign:"center",
   },
 
-  input1: {
-    width: "100%",
-    height: 40,
-    borderStyle: "solid",
-    borderWidth: 2,
-    borderColor:"#0077b6",
-    fontSize: 18,
-    paddingStart: 10,
-    borderRadius: 10,
-    fontFamily: "Montserrat-Regular",
-  },
-
-  pressable1: {
-    backgroundColor: "#023e8a",
-    height: 50,
-    borderRadius: 10,
-    marginTop: 10,
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection:"row",
-    columnGap:10,
-  },
-
-  text3: {
-    fontFamily: "Montserrat-Bold",
-    fontSize: 20,
-    color: "white",
-  },
-
-  text4: {
-    fontFamily: "Montserrat-Bold",
-    fontSize: 18,
-    color: "#023e8a",
+  text3:{
+    fontSize:18,
+    fontFamily:"Montserrat-Bold",
+    marginTop:10,
   },
 
   image1:{
     width:100,
     height:100,
     alignSelf:"center",
-    marginBottom:15,
-  }
+  },
+
+  input1:{
+    width:"100%",
+    height:50,
+    borderStyle:"solid",
+    borderWidth:2,
+    borderRadius:15,
+    paddingStart:10,
+  },
+
+  text4:{
+    fontSize:16,
+    fontFamily:"Montserrat-Bold",
+    color:"white",
+  },
+
+  Pressable1:{
+    height:50,
+    backgroundColor:"blue",
+    justifyContent:"center",
+    alignItems:"center",
+    borderRadius:10,
+    marginTop:10,
+    flexDirection:"row",
+    columnGap:10,
+  },
+
+  Pressable2:{
+    height:25,
+    justifyContent:"center",
+    alignItems:"center",
+    borderRadius:10,
+    marginTop:10,
+  },
+
+  text5:{
+    fontSize:15,
+    fontFamily:"Montserrat-Bold",
+  },
 
 });
