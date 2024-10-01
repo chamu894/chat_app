3390f3f96bc91f2451729b74fdfaba685a1442fe
chamudith bandara <chamudithbuddima@gmail.com>
2024-10-01 09:08:12 +0530

    new

--- App.js
+++ App.js
@@ -7,14 +7,19 @@ import {
   TextInput,
   Pressable,
   Alert,
-  Image,
+  Button,
 } from "react-native";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { FontAwesome6 } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import { Image } from "expo-image";
+import * as ImagePicker from "expo-image-picker";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [getImage, setImage] = useState(null);
+
   const [loaded, error] = useFonts({
     "Montserrat-Bold": require("./assets/fonts/Montserrat-Bold.ttf"),
     "Montserrat-Light": require("./assets/fonts/Montserrat-Light.ttf"),
@@ -33,110 +38,130 @@ export default function App() {
 
   const logoPath = require("./assets/images/main.jpeg");
 
-
   return (
-    <View style={stylesheet.view1}>
+    <LinearGradient colors={["#4c669f", "#3b5998"]} style={stylesheet.view1}>
+      <Button
+        title="Select Image"
+        onPress={async () => {
+          let result = await ImagePicker.launchImageLibraryAsync(
+            {
 
-      <Image style={stylesheet.image1} source={logoPath}/>
+            }
+          );
+
+          if (!result.canceled) {
+            setImage(result.assets[0].uri);
+          }
+        }}
+      />
+
+      <Image style={stylesheet.image1} source={getImage} contentFit="contain" />
 
       <Text style={stylesheet.text1}>Create Account</Text>
 
       <Text style={stylesheet.text2}>Hello! Welcome to Smart Chat</Text>
 
       <Text style={stylesheet.text3}>Mobile</Text>
-      <TextInput style={stylesheet.input1} inputMode={"tel"}/>
+      <TextInput style={stylesheet.input1} inputMode={"tel"} />
 
       <Text style={stylesheet.text3}>First Name</Text>
-      <TextInput style={stylesheet.input1} inputMode={"text"}/>
+      <TextInput style={stylesheet.input1} inputMode={"text"} />
 
       <Text style={stylesheet.text3}>Last Name</Text>
-      <TextInput style={stylesheet.input1} inputMode={"text"}/>
+      <TextInput style={stylesheet.input1} inputMode={"text"} />
 
       <Text style={stylesheet.text3}>Password</Text>
-      <TextInput style={stylesheet.input1} secureTextEntry={true} inputMode={"text"}/>
-      
+      <TextInput
+        style={stylesheet.input1}
+        secureTextEntry={true}
+        inputMode={"text"}
+      />
+
       <Pressable style={stylesheet.Pressable1}>
-        <FontAwesome6 name={"right-to-bracket"} color={"white"} size={20}/>
+        <FontAwesome6 name={"right-to-bracket"} color={"white"} size={20} />
         <Text style={stylesheet.text4}>Sign Up</Text>
       </Pressable>
 
-      <Pressable style={stylesheet.Pressable2}>
+      <Pressable
+        style={stylesheet.Pressable2}
+        onPress={() => {
+          Alert.alert("Message", "Go To Sign In");
+        }}
+      >
         <Text style={stylesheet.text5}>Already Registered? Go To Sign In</Text>
       </Pressable>
-
-    </View>
+    </LinearGradient>
   );
 }
 
 const stylesheet = StyleSheet.create({
   view1: {
     flex: 1,
-    justifyContent:"center",
-    paddingHorizontal:20,
-    rowGap:10,
+    justifyContent: "center",
+    paddingHorizontal: 20,
+    rowGap: 10,
   },
 
-  text1:{
-    fontSize:32,
-    fontFamily:"Montserrat-Bold",
-    textAlign:"center",
+  text1: {
+    fontSize: 32,
+    fontFamily: "Montserrat-Bold",
+    textAlign: "center",
   },
 
-  text2:{
-    fontSize:20,
-    fontFamily:"Montserrat-Regular",
-    textAlign:"center",
+  text2: {
+    fontSize: 20,
+    fontFamily: "Montserrat-Regular",
+    textAlign: "center",
   },
 
-  text3:{
-    fontSize:18,
-    fontFamily:"Montserrat-Bold",
-    marginTop:10,
+  text3: {
+    fontSize: 18,
+    fontFamily: "Montserrat-Bold",
+    marginTop: 10,
   },
 
-  image1:{
-    width:100,
-    height:100,
-    alignSelf:"center",
+  image1: {
+    width: 100,
+    height: 100,
+    alignSelf: "center",
   },
 
-  input1:{
-    width:"100%",
-    height:50,
-    borderStyle:"solid",
-    borderWidth:2,
-    borderRadius:15,
-    paddingStart:10,
+  input1: {
+    width: "100%",
+    height: 50,
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderRadius: 15,
+    paddingStart: 10,
   },
 
-  text4:{
-    fontSize:16,
-    fontFamily:"Montserrat-Bold",
-    color:"white",
+  text4: {
+    fontSize: 16,
+    fontFamily: "Montserrat-Bold",
+    color: "white",
   },
 
-  Pressable1:{
-    height:50,
-    backgroundColor:"blue",
-    justifyContent:"center",
-    alignItems:"center",
-    borderRadius:10,
-    marginTop:10,
-    flexDirection:"row",
-    columnGap:10,
+  Pressable1: {
+    height: 50,
+    backgroundColor: "blue",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    marginTop: 10,
+    flexDirection: "row",
+    columnGap: 10,
   },
 
-  Pressable2:{
-    height:25,
-    justifyContent:"center",
-    alignItems:"center",
-    borderRadius:10,
-    marginTop:10,
+  Pressable2: {
+    height: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    marginTop: 10,
   },
 
-  text5:{
-    fontSize:15,
-    fontFamily:"Montserrat-Bold",
+  text5: {
+    fontSize: 15,
+    fontFamily: "Montserrat-Bold",
   },
-
 });
