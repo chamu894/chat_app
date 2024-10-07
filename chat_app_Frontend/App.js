bdbac313598807a39a5a99737aaea609302f9a42
chamudith bandara <chamudithbuddima@gmail.com>
2024-10-07 12:17:22 +0530

    new

--- chat_app_Frontend/App.js
+++ chat_app_Frontend/App.js
@@ -14,6 +14,7 @@ import { FontAwesome6 } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
 import { Image } from "expo-image";
 import * as ImagePicker from "expo-image-picker";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 SplashScreen.preventAutoHideAsync();
 
