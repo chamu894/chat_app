398e465c37d9f5c26a7ce2a58cbfb87e2b42b6a5
chamudith bandara <chamudithbuddima@gmail.com>
2024-12-15 22:39:20 +0530

    new

---
+++ chat_app_Frontend/app/index copy.js
@@ -0,0 +1,266 @@
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Pressable,
+  Alert,
+  ScrollView,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { useState, useEffect } from "react";
+import { FontAwesome6 } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import { Image } from "expo-image";
+import * as ImagePicker from "expo-image-picker";
+import { registerRootComponent } from "expo";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { router } from "expo-router";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function index() {
+  const [getMobile, setMobile] = useState("");
+  const [getPassword, setPassword] = useState("");
+  const [getName, setName] = useState("");
+
+  const [loaded, error] = useFonts({
+    "Montserrat-Bold": require("../assets/fonts/Montserrat-Bold.ttf"),
+    "Montserrat-Light": require("../assets/fonts/Montserrat-Light.ttf"),
+    "Montserrat-Regular": require("../assets/fonts/Montserrat-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    async function checkuser() {
+      try {
+        let userJson = await AsyncStorage.getItem("user");
+        if (userJson != null) {
+          router.replace("/home");
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    checkuser();
+  },[]
+);
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  const logoPath = require("../assets/images/main.jpeg");
+
+  const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+
+  return (
+    <LinearGradient colors={["#fff", "#fff"]} style={stylesheet.view1}>
+      <StatusBar hidden={true} />
+      <ScrollView style={stylesheet.scrollview1}>
+        <View style={stylesheet.view2}>
+          <Image
+            style={stylesheet.image1}
+            source={logoPath}
+            contentFit="contain"
+          />
+
+          <Text style={stylesheet.text1}>Sign In</Text>
+
+          <Text style={stylesheet.text2}>Hello! Welcome to Smart Chat</Text>
+
+          <View style={stylesheet.avatar1}>
+            <Text style={stylesheet.text6}>{getName}</Text>
+          </View>
+
+          <Text style={stylesheet.text3}>Mobile</Text>
+          <TextInput
+            style={stylesheet.input1}
+            inputMode={"tel"}
+            onChangeText={(text) => {
+              setMobile(text);
+            }}
+            onEndEditing={async () => {
+              if (getMobile.length == 10) {
+                let response = await fetch(
+                  `${apiUrl}GetLetters?mobile=${getMobile}`
+                );
+
+                if (response.ok) {
+                  let json = await response.json();
+                  setName(json.letters);
+                }
+              }
+            }}
+          />
+
+          <Text style={stylesheet.text3}>Password</Text>
+          <TextInput
+            style={stylesheet.input1}
+            secureTextEntry={true}
+            inputMode={"text"}
+            onChangeText={(text) => {
+              setPassword(text);
+            }}
+          />
+
+          <Pressable
+            style={stylesheet.Pressable1}
+            onPress={async () => {
+              let response = await fetch(
+                `${apiUrl}UserLogin`,
+                {
+                  method: "POST",
+                  body: JSON.stringify({
+                    mobile: getMobile,
+                    password: getPassword,
+                  }),
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                }
+              );
+              
+              if (response.ok) {
+                
+                let json = await response.json();
+
+                if (json.success) {
+                  let user = json.user;
+                  Alert.alert(
+                    "Success",
+                    "Hi " + user.frist_name + ", " + json.message
+                  );
+
+                  try {
+                    await AsyncStorage.setItem("user", JSON.stringify(user));
+                    router.replace("/home");
+                  } catch (e) {
+                    Alert.alert("Error", "unable to prosses your request");
+                  }
+                } else {
+                  Alert.alert("Error", json.message);
+                }
+              }else{
+                console.log("Erroe1");
+              }
+            }}
+          >
+            <FontAwesome6 name={"right-to-bracket"} color={"white"} size={20} />
+            <Text style={stylesheet.text4}>Sign In</Text>
+          </Pressable>
+
+          <Pressable
+            style={stylesheet.Pressable2}
+            onPress={() => {
+              router.replace("/signup");
+            }}
+          >
+            <Text style={stylesheet.text5}>New User? Go To Sign Up</Text>
+          </Pressable>
+        </View>
+      </ScrollView>
+    </LinearGradient>
+  );
+}
+
+const stylesheet = StyleSheet.create({
+  view1: {
+    flex: 1,
+    justifyContent: "center",
+    marginTop: 50,
+  },
+
+  text1: {
+    fontSize: 32,
+    fontFamily: "Montserrat-Bold",
+    textAlign: "center",
+  },
+
+  text2: {
+    fontSize: 20,
+    fontFamily: "Montserrat-Regular",
+    textAlign: "center",
+  },
+
+  text3: {
+    fontSize: 18,
+    fontFamily: "Montserrat-Bold",
+    marginTop: 10,
+  },
+
+  image1: {
+    width: 100,
+    height: 100,
+    alignSelf: "center",
+    // borderRadius:100,
+  },
+
+  input1: {
+    width: "100%",
+    height: 50,
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderRadius: 15,
+    paddingStart: 10,
+  },
+
+  text4: {
+    fontSize: 16,
+    fontFamily: "Montserrat-Bold",
+    color: "white",
+  },
+
+  Pressable1: {
+    height: 50,
+    backgroundColor: "blue",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    marginTop: 10,
+    flexDirection: "row",
+    columnGap: 10,
+  },
+
+  Pressable2: {
+    height: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    marginTop: 10,
+  },
+
+  text5: {
+    fontSize: 15,
+    fontFamily: "Montserrat-Bold",
+  },
+
+  avatar1: {
+    width: 90,
+    height: 90,
+    borderRadius: 45,
+    backgroundColor: "#D1D1D1",
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+
+  view2: {
+    flex: 1,
+    paddingHorizontal: 18,
+    rowGap: 5,
+  },
+
+  text6: {
+    fontSize: 50,
+    fontFamily: "Montserrat-Bold",
+    color: "blue",
+    alignSelf: "center",
+  },
+});
--- chat_app_backend/src/java/controller/SaveChat.java
+++ chat_app_backend/src/java/controller/SaveChat.java
@@ -6,16 +6,16 @@ import entity.Chat;
 import entity.Chat_Status;
 import entity.User;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Date;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.crypto.Data;
 import model.HibernateUtil;
 import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 @WebServlet(name = "SaveChat", urlPatterns = {"/SaveChat"})
 public class SaveChat extends HttpServlet {
@@ -28,23 +28,50 @@ public class SaveChat extends HttpServlet {
 
             JsonObject reuestObject = gson.fromJson(req.getReader(), JsonObject.class);
 
-            Session session = HibernateUtil.getSessionFactory().openSession();
+            JsonObject responseObject = new JsonObject();
+            responseObject.addProperty("status", false);
 
-            User fromUser = (User) session.get(User.class, reuestObject.get("fromUser").getAsInt());
-            User toUser = (User) session.get(User.class, reuestObject.get("toUser").getAsInt());
+            if (reuestObject != null && !reuestObject.get("msg").getAsString().isEmpty()) {
+                Session session = HibernateUtil.getSessionFactory().openSession();
 
-            Chat_Status status = (Chat_Status) session.get(Chat_Status.class, 2);
+                User fromUser = (User) session.get(User.class, reuestObject.get("fromUser").getAsInt());
+                User toUser = (User) session.get(User.class, reuestObject.get("toUser").getAsInt());
 
-            Chat chat = new Chat();
-            chat.setChat_status(status);
-            chat.setDate_time(new Date());
-            chat.setFrom_user(fromUser);
-            chat.setTo_user(toUser);
-            chat.setMessage(reuestObject.get("msg").getAsString());
+                if (fromUser != null && toUser != null) {
 
-            session.save(chat);
+                    Chat_Status status = (Chat_Status) session.get(Chat_Status.class, 2);
 
-            session.beginTransaction().commit();
+                    List<Chat> chatList = (List<Chat>) session.createCriteria(Chat.class).add(Restrictions.eq("chat_status", status)).
+                            add(Restrictions.eq("to_user", fromUser)).list();
+
+                    if (!chatList.isEmpty()) {
+                        
+                        Chat_Status status2 = (Chat_Status)session.get(Chat_Status.class, 1);
+                        
+                        for (Chat chat : chatList) {
+                            chat.setChat_status(status2);
+                            session.update(chat);
+                        }
+                    }
+
+                    Chat chat = new Chat();
+                    chat.setChat_status(status);
+                    chat.setDate_time(new Date());
+                    chat.setFrom_user(fromUser);
+                    chat.setTo_user(toUser);
+                    chat.setMessage(reuestObject.get("msg").getAsString());
+
+                    session.save(chat);
+
+                    session.beginTransaction().commit();
+
+                    responseObject.addProperty("status", true);
+                }
+                session.close();
+            }
+
+            resp.setContentType("application/json");
+            resp.getWriter().write(gson.toJson(responseObject));
 
         } catch (Exception e) {
             e.printStackTrace();
